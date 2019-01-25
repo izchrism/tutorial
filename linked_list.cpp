c827d4607669d9fb257fd91d91e550d169fcc944
Christian Maschka <christianmaschka@dyn3137-56.wlan.ic.ac.uk>
2019-01-25 10:49:35 +0000

    testing

---
+++ linked_list.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+struct intlist{
+    int val;
+    intlist* next;
+};
+
+
+int main(){
+    intlist* list = NULL;
+    int number_of_elements, element_to_add;
+
+    std::cout << "How many elements would you like to add? " << std::endl;
+    std::cin >> number_of_elements;
+    std::cout <<"HELlO"<<std::endl;
+
+    for (int i = 0; i < number_of_elements; i++){
+
+        std::cout<< "Enter Element " << i+1 << " :" <<std::endl;
+        std::cin >> element_to_add;
+
+        intlist* tmp = new intlist;
+        (*tmp).val = element_to_add;
+        tmp->next = list;
+        list = tmp;
+    }
+    intlist* print_list = list;
+    while(print_list!= NULL){
+        std::cout << print_list -> val << std::endl;
+        print_list = print_list->next;
+    }
+
+    while(list != NULL){
+        intlist* tmpdl = list-> next;
+        delete list;
+        list = tmpdl;
+    }
+    std::cout <<"Bye!"<<std::endl;
+}
